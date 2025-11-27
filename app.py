167efd7267d9434920a9e9f2c86542a4cadc7b57
bryan <hargoescorgo5@gmail.com>
2025-11-27 03:29:04 -0600

    fix: add print statement so u know in logs when it starts up lol

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from getreports import fetch_reports
 
 app = FastAPI()
 
+print("listening on port 6970 (hopefully)")
+
 @app.post("/findmy/")
 async def create_item(request: TrackerRequest):
     return await fetch_reports(request)
